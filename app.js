861dff093346153bb5855ecdef8a7dd8239497c6
TommyFu <liok3187@gmail.com>
2017-11-10 17:37:16 +0800

    Create folder

--- app.js
+++ app.js
@@ -56,21 +56,17 @@ app.get('/file', function(req, res) {
       } catch(ex){
         continue;
       }
+      let item = {
+        path: path.join(subPath, files[i]),
+        name: files[i],
+        date: stat.ctime
+      };
       if(stat.isDirectory()) {
-        ret.push({
-          path: path.join(subPath, files[i]),
-          type: FILE_TYPE.FOLDER,
-          name: files[i],
-          date: stat.ctime
-        });
+        item.type = FILE_TYPE.FOLDER;
       }else if(stat.isFile){
-        ret.push({
-          path: path.join(subPath, files[i]),
-          type: FILE_TYPE.FILE,
-          name: files[i],
-          date: stat.ctime
-        });
+        item.type = FILE_TYPE.FILE;
       }
+      ret.push(item);
     }
     res.status(200);
     res.json(ret);
@@ -101,8 +97,36 @@ app.post('/upload', upload.single('file'), function (req, res, next) {
     res.end('Upload Failed');
     return ;
   }
-  
-})
+});
+
+app.post('/createFolder', function(req, res) {
+  let folderPath = req.body.path;
+  let folderName = req.body.name;
+
+  if(folderPath && folderName){
+    let fullPath = path.join(config.publicFolder, folderPath, folderName);
+    let exists = fs.existsSync(fullPath);
+    if(!exists){
+      fs.mkdir(fullPath, function(err){
+        if(err){
+          res.statusCode = 500;
+          res.end(err);
+          return;
+        }
+        res.status(200);
+        res.json({});
+        return;
+      })
+    }else{
+      res.statusCode = 500;
+      res.end('This folder is already existing.');
+    }
+  }else{
+    res.statusCode = 500;
+    res.end('Create folder failed');
+  }
+
+});
 
 http.createServer(app).listen(app.get('port'), function() {
   console.log("Server listening on port " + app.get('port'));
--- public/src/list.js
+++ public/src/list.js
@@ -1,14 +1,119 @@
 requirejs([], function() {
   'use strict';
 
-  $(document).ready(function(){
+  function getUrlParam(name) {
+    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+    var r = window.location.search.substr(1).match(reg);
+    if (r != null) return unescape(r[2]); return null;
+  }
 
-    function getUrlParam(name) {
-      var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
-      var r = window.location.search.substr(1).match(reg);
-      if (r != null) return unescape(r[2]); return null;
+  function buildList(res){
+    var $ul = $($('.file-list > ul')[0]);
+    for(var i = 0; i < res.length; i++){
+      var curr = res[i];
+      var $li = $(document.createElement('li'));
+      $li.addClass('list-group-item');
+      $ul.append($li);
+
+      var $icon = $(document.createElement('span'));
+      if(curr.type === 'folder'){
+        $icon.addClass('oi oi-folder file-list-folder');
+      }else{
+        $icon.addClass('oi oi-file file-list-file');
+      }
+      $icon.addClass('file-list-icon');
+      $li.append($icon);
+
+      var $a = $(document.createElement('a'));  
+      $a.html(curr.name);
+      $li.append($a);
+      if(curr.type === 'folder'){
+        $a.attr('href', '/list.html?path=' + curr.path);
+      }else{
+        $a.attr('href', '/file?path=' + curr.path);
+      }
+    }
+  }
+
+  function refresh(path){
+    var redirectUrl = "list.html"; 
+    if(path){
+      redirectUrl += "?path=" + path;
+    }
+    window.location.href = redirectUrl;
+  }
+
+  function createFolder(path){
+    var folderName = $("#file-folder-name").val().trim();
+    var data = {
+      path: path ? path : '/',
+      name: folderName
+    };
+    var dataStr = '';
+    try{
+      dataStr =JSON.stringify(data);
+    }catch(ex){}
+    $.ajax({
+      type: 'POST',
+      url: '/createFolder',
+      data: dataStr,
+      dataType: "json",
+      contentType: "application/json; charset=utf-8",
+      success: function (arg) {
+        refresh(path);
+      },
+      error: function (arg) {
+        if(arg.responseText){
+          alert(arg.responseText);
+        }else{
+          alert('Create folder failed');
+        }
+      }
+    });
+  }
+
+  function upload(path){
+    var xhrOnProgress = function(cb) {
+        xhrOnProgress.onprogress = cb;
+        return function() {
+          var xhr = $.ajaxSettings.xhr();
+          if (xhrOnProgress.onprogress 
+            && typeof xhrOnProgress.onprogress === 'function'
+            && xhr.upload) {
+              xhr.upload.onprogress = xhrOnProgress.onprogress;
+          }
+          return xhr;
+        }
     }
 
+    var file = $("#file-uploader-selector")[0].files[0];
+    var form = new FormData();
+    form.append('file', file);
+    form.append('path', path ? path : '/');
+    $.ajax({
+      type: 'POST',
+      url: '/upload',
+      data: form,
+      processData: false,
+      contentType: false,
+      xhr: xhrOnProgress(function(e){
+        var percent = e.loaded / e.total;
+        $("#file-uploader-bar").css("width", percent * 100 + '%');
+      }),
+      success: function (arg) {
+        refresh(path);
+      },
+      error: function (arg) {
+        if(arg.responseText){
+          alert(arg.responseText);
+        }else{
+          alert('Upload failed');
+        }
+      }
+    });
+  }
+
+  $(document).ready(function(){
     var path = getUrlParam('path');
     var url = path ? ('/file?path=' + path) : '/file';
 
@@ -16,36 +121,24 @@ requirejs([], function() {
       url: url,
       success: function(res){
         if(res && res.length){
-          var $ul = $($('.file-list > ul')[0]);
-          for(var i = 0; i < res.length; i++){
-            var curr = res[i];
-            var $li = $(document.createElement('li'));
-            $li.addClass('list-group-item');
-            $ul.append($li);
-
-            var $icon = $(document.createElement('span'));
-            if(curr.type === 'folder'){
-              $icon.addClass('oi oi-folder');
-            }else{
-              $icon.addClass('oi oi-file');
-            }
-            $icon.addClass('file-list-icon');
-            $li.append($icon);
-
-            var $a = $(document.createElement('a'));  
-            $a.html(curr.name);
-            $li.append($a);
-            if(curr.type === 'folder'){
-              $a.attr('href', '/list.html?path=' + curr.path);
-            }else{
-              $a.attr('href', '/file?path=' + curr.path);
-            }
-          }
+          buildList(res);
         }
+
+        $("#file-folder-button").on('click', function (e) {
+          e.preventDefault();
+          var $folderName = $("#file-folder-name");
+          if(!$folderName || $folderName.val().trim().length === 0){
+            alert('Please input the folder name.');
+            return;
+          }
+          createFolder(path);
+        });
 
         $("#file-uploader-btn").on('click', function (e) {
           e.preventDefault();
-          if(!$("#file-uploader-selector")[0] || !$("#file-uploader-selector")[0].files[0]){
+          var $selector = $("#file-uploader-selector");
+          if(!$selector[0] || !$selector[0].files
+            || !$selector[0].files[0]){
             alert('Please select a file.');
             return;
           }
@@ -53,49 +146,8 @@ requirejs([], function() {
         });
 
         $("#file-uploader-selector").change(function () {
-          $("#file-uploader-bar").css("width", 0);
+          $("#file-uploader-bar").css('width', 0);
         });
-  
-        function upload(path) {
-          var xhrOnProgress = function (fn) {
-              xhrOnProgress.onprogress = fn;
-              return function () {
-                var xhr = $.ajaxSettings.xhr();
-                if (xhrOnProgress.onprogress 
-                  && typeof xhrOnProgress.onprogress === 'function'
-                  && xhr.upload) {
-                    xhr.upload.onprogress = xhrOnProgress.onprogress;
-                }
-                return xhr;
-              }
-          }
-
-          var file = $("#file-uploader-selector")[0].files[0];
-          var form = new FormData();
-          form.append('file', file);
-          form.append('path', path ? path : '/');
-          $.ajax({
-            type: 'POST',
-            url: '/upload',
-            data: form,
-            processData: false,
-            contentType: false,
-            xhr: xhrOnProgress(function (e) {
-              var percent = e.loaded / e.total;
-              $("#file-uploader-bar").css("width", percent * 100 + '%');
-            }),
-            success: function (arg) {
-              var redirectUrl = "list.html"; 
-              if(path){
-                redirectUrl += "?path=" + path;
-              }
-              window.location.href = redirectUrl;
-            },
-            error: function (arg) {
-              alert('Upload failed');
-            }
-          })
-        }
       },
       error: function(e){
         alert('List folder failed');
